7a0dcf0ab4d16a97396f1988034ad17ea1878e46
onigra <3280467rec@gmail.com>
2018-03-04 17:05:33 +0900

    use default route table

--- route_table.tf
+++ route_table.tf
@@ -19,8 +19,8 @@ resource "aws_route_table_association" "private" {
   route_table_id = "${aws_route_table.private.id}"
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+resource "aws_default_route_table" "public" {
+  default_route_table_id = "${aws_vpc.vpc.default_route_table_id}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -32,10 +32,16 @@ resource "aws_route_table" "public" {
     Environment = "${var.environment}"
     Name        = "${var.environment}-${var.app_name}-public"
     Access      = "public"
+    Default     = "true"
   }
 }
 
 resource "aws_route_table_association" "public" {
   subnet_id      = "${aws_subnet.public.id}"
-  route_table_id = "${aws_route_table.public.id}"
+  route_table_id = "${aws_default_route_table.public.id}"
+}
+
+resource "aws_main_route_table_association" "main" {
+  vpc_id         = "${aws_vpc.vpc.id}"
+  route_table_id = "${aws_default_route_table.public.id}"
 }
