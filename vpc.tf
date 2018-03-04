3b6e70d44067c255f6995e3262239eb7863bada9
onigra <3280467rec@gmail.com>
2018-03-04 17:50:05 +0900

    enable dns support

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block         = "10.0.0.0/16"
+  enable_dns_support = true
 
   tags {
     Name = "${var.environment}-${var.app_name}"
