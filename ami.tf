d2ddaa1eb887cfa3d75582a62bfbf5541357d0a2
onigra <3280467rec@gmail.com>
2018-03-05 20:34:41 +0900

    amiを切り出した

---
+++ ami.tf
@@ -0,0 +1,8 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126"]
+  }
+}
--- bastion.tf
+++ bastion.tf
@@ -1,12 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20180126"]
-  }
-}
-
 resource "aws_instance" "bastion" {
   ami                         = "${data.aws_ami.ubuntu.id}"
   instance_type               = "t2.micro"
