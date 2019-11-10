641b724d362910b7554eb2de0824e7094def6121
onigra <3280467rec@gmail.com>
2019-11-10 17:34:51 +0900

    踏み台は不要

--- staging/sg.tf
+++ staging/sg.tf
@@ -1,29 +1,3 @@
-resource "aws_security_group" "bastion" {
-  name        = "bastion"
-  description = "bastion"
-  vpc_id      = "${aws_vpc.vpc.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = "${var.bastion_allow_ip_list}"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name        = "${var.short_env}-${var.app_name}-bastion"
-    Environment = "${var.environment}"
-    App         = "${var.app_name}"
-  }
-}
-
 resource "aws_security_group" "alb" {
   name        = "${var.short_env}-${var.app_name}-alb"
   description = "${var.short_env}-${var.app_name}-alb"
