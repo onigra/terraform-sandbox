077c0c62debbd34f3f2d3a1856d56372064c45cb
onigra <3280467rec@gmail.com>
2019-11-10 13:28:02 +0900

    余計なbucketを削除

--- staging/s3.tf
+++ staging/s3.tf
@@ -6,25 +6,3 @@ resource "aws_s3_bucket" "terraform_state" {
     Name = "${var.app_name}-terraform-state"
   }
 }
-
-resource "aws_s3_bucket" "assets" {
-  bucket = "${var.short_env}-${var.app_name}-static"
-  acl    = "private"
-
-  tags {
-    Name        = "${var.short_env}-${var.app_name}-static"
-    Environment = "${var.environment}"
-    App         = "${var.app_name}"
-  }
-}
-
-resource "aws_s3_bucket" "log" {
-  bucket = "${var.short_env}-${var.app_name}-log"
-  acl    = "private"
-
-  tags {
-    Name        = "${var.short_env}-${var.app_name}-log"
-    Environment = "${var.environment}"
-    App         = "${var.app_name}"
-  }
-}
